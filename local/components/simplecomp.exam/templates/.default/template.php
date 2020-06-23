ef422af30123a513e3ebab5b2654d576f6eff715
Artem Anisimov <a.anisimov@runetsoft.ru>
2020-06-23 13:42:10 +0300

    [ex2-58] Добавить управление элементами – «Эрмитаж» в созданный простой компонент «Каталог товаров»

--- local/components/simplecomp.exam/templates/.default/template.php
+++ local/components/simplecomp.exam/templates/.default/template.php
@@ -7,16 +7,36 @@ use Bitrix\Main\Localization\Loc; ?>
     <b><?= Loc::getMessage("MESS_CATALOG_TITLE") ?></b>
     <ul>
         <? foreach ($arResult["NEWS"] as $newsItem): ?>
+
+
+            <? // ex2-58
+            $sElementId = $arResult["ID"] . $newsItem["ID"];
+
+            $this->AddEditAction($sElementId, $newsItem["EDIT_LINK"],
+                CIBlock::GetArrayByID($newsItem["IBLOCK_ID"], "ELEMENT_EDIT"));
+            $this->AddDeleteAction($sElementId, $newsItem["EDIT_LINK"],
+                CIBlock::GetArrayByID($newsItem["IBLOCK_ID"], "ELEMENT_DELETE"),
+                array("CONFIRM" => GetMessage("EX2_58_ELEMENT_DELETE_CONFIRM")));
+            ?>
+
+
+        <div id="<?= $this->GetEditAreaId($sElementId)?>">
             <li>
                 <?= "<b>{$newsItem["NAME"]}</b> - {$newsItem["ACTIVE_FROM"]}"?> (<?= implode(", ", $newsItem["SECTION_NAME"]) ?>)
                 <ul>
                     <? foreach ($newsItem["PRODUCTS"] as $product): ?>
+
+
+
                         <li>
                             <?= "{$product["NAME"]} - {$product["PROPERTY_PRICE_VALUE"]} - {$product["PROPERTY_MATERIAL_VALUE"]} - {$product["PROPERTY_ARTNUMBER_VALUE"]}" ?>
                         </li>
+
                     <? endforeach; ?>
                 </ul>
+
             </li>
+        </div>
         <? endforeach; ?>
     </ul>
 </div>
