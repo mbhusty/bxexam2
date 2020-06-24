66a2e484c4463f449992f9e6addf28dffbcba9e1
Artem Anisimov <a.anisimov@runetsoft.ru>
2020-06-24 11:45:18 +0300

    [ex2-10] Оценить скорость работы сайта – найти самую долгую страницу и самый долгий компонент

---
+++ ex2/time_control/ex2-10.php
@@ -0,0 +1,17 @@
+<?
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+$APPLICATION->SetTitle("ex2-88");
+?>
+	<p>
+		Страница:
+		<a href="http://bxexam2.loc/bitrix/admin/perfmon_hit_list.php?lang=ru&set_filter=Y&find_script_name=%2Fproducts%2Findex.php">
+			/products/index.php
+		</a>
+		<br>
+		Доля нагрузки: 42.66%
+	</p>
+
+	<p>
+		bitrix:catalog: 0.1192 с
+	</p>
+<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
